81cf20380c33a39d0815aa1553f0032f1f4e4c23
Katsuki <Katsuki_Sato0130@icloud.com>
2022-03-23 18:49:36 +0900

    Add arrows

--- src/lib/Tooltip.svelte
+++ src/lib/Tooltip.svelte
@@ -9,17 +9,17 @@
 
 	const placeMap = {
 		top: { xAxis: -50, yAxis: -110, top: -15, left: 0 },
-		bottom: { xAxis: -50, yAxis: 0, top: 15, left: 0},
+		bottom: { xAxis: -50, yAxis: 0, top: 15, left: 0 },
 		left: { xAxis: -110, yAxis: -50, top: 0, left: -15 },
 		right: { xAxis: 0, yAxis: -50, top: 0, left: 15 }
 	};
 
-	const {xAxis, yAxis, top, left} = placeMap[place]
+	const { xAxis, yAxis, top, left } = placeMap[place];
 </script>
 
 <div
 	style="top: {mouseY + top}px; left: {mouseX + left}px; transform: translate({xAxis}%, {yAxis}%);"
-	class="tooltip"
+	class="tooltip {place}"
 >
 	{body}
 </div>
@@ -31,5 +31,49 @@
 		padding: 4px;
 		background: #fff;
 		white-space: nowrap;
+	}
+
+	.top::after {
+		content: ' ';
+		position: absolute;
+		top: 100%; /* At the bottom of the tooltip */
+		left: 50%;
+		margin-left: -5px;
+		border-width: 5px;
+		border-style: solid;
+		border-color: black transparent transparent transparent;
+	}
+
+	.bottom::after {
+		content: ' ';
+		position: absolute;
+		bottom: 100%; /* At the top of the tooltip */
+		left: 50%;
+		margin-left: -5px;
+		border-width: 5px;
+		border-style: solid;
+		border-color: transparent transparent black transparent;
+	}
+
+	.right::after {
+		content: ' ';
+		position: absolute;
+		top: 50%;
+		right: 100%; /* To the left of the tooltip */
+		margin-top: -5px;
+		border-width: 5px;
+		border-style: solid;
+		border-color: transparent black transparent transparent;
+	}
+	
+	.left::after {
+		content: ' ';
+		position: absolute;
+		top: 50%;
+		left: 100%; /* To the right of the tooltip */
+		margin-top: -5px;
+		border-width: 5px;
+		border-style: solid;
+		border-color: transparent transparent transparent black;
 	}
 </style>
